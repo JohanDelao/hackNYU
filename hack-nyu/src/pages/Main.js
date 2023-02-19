9db368f70189d0e970f7e0dba58876a406201a30
Johan Delao <johandelao10@gmail.com>
2023-02-19 04:44:16 -0500

    improvement on posts

--- hack-nyu/src/pages/Main.js
+++ hack-nyu/src/pages/Main.js
@@ -50,6 +50,46 @@ const Main = () => {
       console.log(error);
     }
   };
+  function getMonthName(monthNumber) {
+    const date = new Date();
+    date.setMonth(monthNumber - 1);
+  
+    return date.toLocaleString('en-US', { month: 'long' });
+  }
+
+  const date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  month = getMonthName(month).toLowerCase();
+  month = month[0] + month[1] + month[2];
+
+  // const getData2 = async (m,d) => {
+  //   const collectionRef = collection(db, "posts");
+  //   const q = query(collectionRef, where('month', '==', m), where('day', '==', d), where('user', '==', person.uid));
+  //   const unsubscribe = onSnapshot(q, (snapshot => {
+  //     setPosts(snapshot.docs.map((doc) => ({...doc.data(), id: doc.id})))
+  //   }))
+  //   return unsubscribe;
+  // }
+
+  function openForm(m, d){
+    console.log(posts)
+    console.log(m)
+    console.log(d)
+    const results = posts.filter((post) => {
+      if(post.day == d && post.month == m){
+        return true;
+      }else{
+        return false;
+      }
+    })
+    let modal = document.getElementById("modal");
+    let first = document.getElementById("questionOneValue");
+    console.log(results)
+    console.log(results[0].questionOneAnswer);
+    first.innerHTML = results[0].questionOneAnswer;
+    modal.classList.add("show");
+  }
 
   return (
     <div className="body1">
@@ -77,35 +117,42 @@ const Main = () => {
               <br></br>
               <div className="formBoxes">
                 <div className="formBox">
-                  <div id="month">feb</div>
-                  <div id="day">18</div>
+                  <div id="month">{month}</div>
+                  <div id="day">{day-6}</div>
                 </div>
                 <div className="formBox">
-                  <div id="month">feb</div>
-                  <div id="day">19</div>
+                  <div id="month">{month}</div>
+                  <div id="day">{day-5}</div>
                 </div>
                 <div className="formBox">
-                  <div id="month">feb</div>
-                  <div id="day">20</div>
+                  <div id="month">{month}</div>
+                  <div id="day">{day-4}</div>
                 </div>
                 <div className="formBox">
-                  <div id="month">feb</div>
-                  <div id="day">21</div>
+                  <div id="month">{month}</div>
+                  <div id="day">{day-3}</div>
                 </div>
                 <div className="formBox">
-                  <div id="month">feb</div>
-                  <div id="day">22</div>
+                  <div id="month">{month}</div>
+                  <div id="day">{day-2}</div>
                 </div>
                 <div className="formBox">
-                  <div id="month">feb</div>
-                  <div id="day">23</div>
+                  <div id="month">{month}</div>
+                  <div id="day">{day-1}</div>
                 </div>
-                <div className="formBox">
-                  <div id="month">feb</div>
-                  <div id="day">24</div>
+                <div className="formBox" onClick={() => openForm(month, day)}>
+                  <div id="month">{month}</div>
+                  <div id="day">{day}</div>
                 </div>
               </div>
               <Form />
+              <div className="modal" id="modal">
+                  <div>
+                    <p id="questionOneValue"></p>
+                    <p id="questionTwoValue"></p>
+                    <p id="questionThreeValue"></p>
+                  </div>
+              </div>
             </div>
             <button onClick={handleSignOut} className="logoutBtn">
               Logout
