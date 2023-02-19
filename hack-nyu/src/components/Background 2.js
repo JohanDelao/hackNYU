73e3dccda17ce9b04a31912953951ac917538140
Angel <aavila002@citymail.cuny.edu>
2023-02-19 05:34:37 -0500

    Create Background 2.js

---
+++ hack-nyu/src/components/Background 2.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+export default function Background() {
+    return(
+        <div className='body1'>
+           
+            <div className="area" >
+                        <ul className="circles">
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                        </ul>
+            </div>
+            
+        </div>
+    )
+}
